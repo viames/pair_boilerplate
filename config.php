640ed934718082ef8c618029592aadd5e45dd19a
Viames Marino <me@viames.it>
2017-04-18 15:46:09 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @version	$Id$
+ * @author	Viames Marino
+ * @package	Pair
+ */
+
+// product
+define ('PRODUCT_VERSION',	'1.0');
+define ('PRODUCT_NAME',		'Pair example');
+define ('BASE_URI',			'/pair_example'); // application subpath on URL
+
+// database
+define('DB_HOST',			'localhost');
+define('DB_NAME',			'pair_example');
+define('DB_USER',			'root');
+define('DB_PASS',			'pw');
+
+// Google Places Api
+define('API_PLACES',		'');
