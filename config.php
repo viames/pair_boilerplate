3fffc6f32d6dc6e71858aa16f40f0d51cf6d1873
Viames Marino <me@viames.it>
2017-10-11 11:42:25 +0200

    Set generic parameters

--- config.php
+++ config.php
@@ -9,13 +9,12 @@
 // product
 define ('PRODUCT_VERSION',	'1.0');
 define ('PRODUCT_NAME',		'Pair example');
-define ('BASE_URI',			'/pair_example'); // application subpath on URL
 
 // database
-define('DB_HOST',			'localhost');
-define('DB_NAME',			'pair_example');
-define('DB_USER',			'root');
-define('DB_PASS',			'pw');
+define('DB_HOST',			'db_host');
+define('DB_NAME',			'db_name');
+define('DB_USER',			'db_user');
+define('DB_PASS',			'db_pass');
 
 // Google Places Api
 define('API_PLACES',		'');
