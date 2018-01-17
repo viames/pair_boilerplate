847f826cae2fe35eee8ae2f647921f72e5044c60
Viames Marino <me@viames.it>
2018-01-17 23:15:48 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 // product
-define ('PRODUCT_VERSION',	'1.0');
-define ('PRODUCT_NAME',		'Pair example');
-define ('BASE_URI',			'/pair_example'); // application subpath on URL
+define ('PRODUCT_VERSION', '1.0');
+define ('PRODUCT_NAME', 'Pair example');
+define ('BASE_URI', '/pair_example'); // application URL subpath
 
 // database
-define('DB_HOST',			'db_host');
-define('DB_NAME',			'db_name');
-define('DB_USER',			'db_user');
-define('DB_PASS',			'db_pass');
+define('DB_HOST', 'db_host');
+define('DB_NAME', 'db_name');
+define('DB_USER', 'db_user');
+define('DB_PASS', 'db_pass');
