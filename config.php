0044fdb31b73324fc9e1ebdcd5ef9902ef5ba4fe
Viames Marino <me@viames.it>
2018-01-17 17:47:58 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define ('PRODUCT_NAME',		'Pair example');
 define ('BASE_URI',			'/pair_example'); // application subpath on URL
 
 // database
-define('DB_HOST',			'localhost');
-define('DB_NAME',			'pair_example');
-define('DB_USER',			'root');
-define('DB_PASS',			'pw');
+define('DB_HOST',			'db_host');
+define('DB_NAME',			'db_name');
+define('DB_USER',			'db_user');
+define('DB_PASS',			'db_pass');
